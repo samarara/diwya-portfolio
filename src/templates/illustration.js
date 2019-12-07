a0b8606b922aa9accd583efadc0ccbecc1e57f33
Sandes de Silva <sandes.samararatne@gmail.com>
2019-12-07 15:19:34 -0500

    fix: add photo and illu templates

---
+++ src/templates/illustration.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const IllustrationTemplate = () => <div>Illu</div>
+
+export default IllustrationTemplate
---
+++ src/templates/photography.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const PhotographyTemplate = () => <div>Photo</div>
+
+export default PhotographyTemplate
