828ab7bd78351a1f41bfee9a34e509694fea75bb
Sandes de Silva <sandes.samararatne@gmail.com>
2019-12-29 21:09:08 -0500

    feat(nav-bar): add active nav item styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'gatsby';
-import github from '../img/github-icon.svg';
-import logo from '../img/logo.svg';
+import React, { useState, useEffect } from 'react'
+import { Link } from 'gatsby'
+import github from '../img/github-icon.svg'
+import logo from '../img/logo.svg'
 
 const Navbar = () => {
-  const [active, setIsactive] = useState(false);
-  const [navBarActiveClass, setNavBarActiveClass] = useState('');
+  const [active, setIsactive] = useState(false)
+  const [navBarActiveClass, setNavBarActiveClass] = useState('')
 
   useEffect(() => {
-    active ? setNavBarActiveClass('is-active') : setNavBarActiveClass('');
-  }, [active]);
+    active ? setNavBarActiveClass('is-active') : setNavBarActiveClass('')
+  }, [active])
 
   const toggleHamburger = () => {
-    setIsactive(!active);
-  };
+    setIsactive(!active)
+  }
 
-  const handleOnClick = () => toggleHamburger();
+  const handleOnClick = () => toggleHamburger()
 
   return (
     <nav
@@ -43,26 +43,46 @@ const Navbar = () => {
         <div id="navMenu" className={`navbar-menu ${navBarActiveClass}`}>
           <div className="navbar-start has-text-centered"></div>
           <div className="navbar-end has-text-centered">
-            <Link className="navbar-item nav-item-override" to="/photography">
+            <Link
+              className="navbar-item nav-item-override"
+              to="/photography"
+              activeClassName="navbar-item-select"
+              partiallyActive
+            >
               Photography
             </Link>
-            <Link className="navbar-item nav-item-override" to="/illustrations">
+            {/* <Link
+              className="navbar-item nav-item-override"
+              to="/illustrations"
+              activeClassName="navbar-item-select"
+              partiallyActive
+            >
               Illustrations
-            </Link>
-            <Link className="navbar-item nav-item-override" to="/case-studies">
+            </Link> */}
+            <Link
+              className="navbar-item nav-item-override"
+              to="/case-studies"
+              activeClassName="navbar-item-select"
+              partiallyActive
+            >
               Case Studies
             </Link>
             {/* <Link className="navbar-item" to="/contact/examples">
                 Form Examples
               </Link> */}
-            <Link className="navbar-item nav-item-override" to="/about">
-              About
+            <Link
+              className="navbar-item nav-item-override"
+              to="/about"
+              activeClassName="navbar-item-select"
+              partiallyActive
+            >
+              About Me
             </Link>
           </div>
         </div>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
