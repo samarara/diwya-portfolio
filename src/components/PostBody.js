0efb7ed8f6cf831920f53a807280daaac6205bf9
Sandes de Silva <sandes.samararatne@gmail.com>
2020-01-08 22:09:33 -0500

    feat(posts): make featured image, title and subtitle in post roll clickable
    closes #18

--- src/components/PostBody.js
+++ src/components/PostBody.js
@@ -4,33 +4,21 @@ import TransitionLink from 'gatsby-plugin-transition-link'
 
 const PostBody = ({ excerpt, to, title }) => (
   <div className="column section">
+    <TransitionLink
+      className="post-title"
+      to={to}
+      exit={{ length: 1 }}
+      entry={{ length: 1 }}
+      trigger={true}
+    >
     <h1 className="title has-text-primary is-size-1-desktop is-size-1-tablet is-size-3-mobile">
-      <TransitionLink
-        className="post-title"
-        to={to}
-        exit={{ length: 1 }}
-        entry={{ length: 1, delay: 0.2 }}
-      >
         {title}
-      </TransitionLink>
     </h1>
 
     <div className="subtitle serif">
       {excerpt}
-      {/* <br /> */}
-      {/* <Link className="button" to={to}>
-        Keep Reading →
-      </Link> */}
     </div>
-    {/* <p className="post-meta">
-      <Link
-        className="title has-text-primary is-size-4"
-        to={to}
-      >
-        {title}
-      </Link>
-      <span> &bull; </span>
-    </p> */}
+      </TransitionLink>
   </div>
 )
 
--- src/components/PostHeader.js
+++ src/components/PostHeader.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import TransitionLink from 'gatsby-plugin-transition-link'
 import FeaturedImage from './FeaturedImage'
 
 const PostHeader = ({ featuredImage, featuredImageAlt, title, to, date }) => (
   <div className="column is-7">
+    <TransitionLink
+      className="post-title"
+      to={to}
+      exit={{ length: 1 }}
+      entry={{ length: 1, delay: 0.2 }}
+    >
     <FeaturedImage image={featuredImage} alt={featuredImageAlt} />
     {/* <p className="post-meta">
       <Link
-        className="title has-text-primary is-size-4"
-        to={to}
+      className="title has-text-primary is-size-4"
+      to={to}
       >
-        {title}
+      {title}
       </Link>
       <span> &bull; </span>
       <span className="subtitle is-size-5 is-block">
-        {date}
+      {date}
       </span>
     </p> */}
+    </TransitionLink>
   </div>
 )
 
