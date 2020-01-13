6eea6017dcdfa14e27aee8b5343649e25a26a994
Sandes de Silva <sandes.samararatne@gmail.com>
2020-01-12 19:38:16 -0500

    fix(404): style the 404 page to be more consistent with other pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import ContentHeader from '../components/ContentHeader'
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <ContentHeader title="Uh Oh!" description="You just hit a route that doesn&#39;t exist... the sadness &#128546;" isFullHeight />
   </Layout>
 )
 
