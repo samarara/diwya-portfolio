1712e5aeb0ffffe765ee45c074a818d6a9733409
Sandes de Silva <sandes.samararatne@gmail.com>
2020-01-24 20:50:57 -0500

    fix(styles): add blockquote selector to purgecss whitlist

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,7 @@ module.exports = {
       options: {
         develop: true, // Activates purging in npm run develop
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
+        whitelist: ['blockquote']
       },
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
